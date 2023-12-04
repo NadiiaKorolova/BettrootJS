bf535ac437c944a84f07dc0ee166a39b84a137f6
Nadiiakorolova <Nadiiakorolova2830@gmail.com>
2023-12-04 13:28:36 +0000

    stucture edition

--- typesJs/test3.js
+++ typesJs/test3.js
@@ -1,5 +1,5 @@
 // task3
-const size = prompt("specify the size of the media");
+// const size = prompt("specify the size of the media");
 const FILE_SIZE = 820 / 1000;
 const task3Result = Math.floor(+size / FILE_SIZE);
 const messageTask3Result = `${task3Result} files with a size of 820 Mb will fit on your storage medium`;
--- typesJs/test4.js
+++ typesJs/test4.js
@@ -1,6 +1,6 @@
 // task4
-const moneyAmount = prompt("enter yhe ammount");
-const chocolatePrice = prompt("enter the price of chocolate");
+// const moneyAmount = prompt("enter yhe ammount");
+// const chocolatePrice = prompt("enter the price of chocolate");
 const resultTask4 = Math.floor(Number(+moneyAmount / +chocolatePrice));
 const restTask4 = Number(moneyAmount % chocolatePrice);
 const messageTask4 = `You can buy ${resultTask4} chocolate bars, the remaining funds will be ${restTask4} $`;
